ae2c1f78313cb9885eff8b193751d82f5be3b5bb
moraissdev <renanbusinessdev@gmail.com>
2025-04-06 18:01:16 -0300

    commit

--- compararDoisNumeros.c
+++ compararDoisNumeros.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    float n1, n2;
+    float n1, n2; 
+
+    printf("Digite um numero: ");
     scanf("%f", &n1);
+
+    printf("Digite um segundo numero: ");
     scanf("%f", &n2);
-    if (n1 < n2) {
-        printf("%0.2f", n1);
+
+    if (n1 > n2) {
+        printf("O menor numero e: %0.2f", n2);
     }
-    else {
-        printf("%0.2f", n2);
+    if(n2 > n1) {
+        printf("O menor numero e: %0.2f", n1);
     }
+    if(n1 == n2) {
+        printf("Os numeros devem ser diferentes!");
+        return 1;
+    }
+
     return 0;
 }
---
+++ comprarTresNumeros.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float n1, n2, n3;
+    printf("Digite um numero: ");
+    scanf("%f", &n1);
+    printf("Digite um segundo numero: ");
+    scanf("%f", &n2);
+    printf("Digite um terceiro numero: ");
+    scanf("%f", &n3);
+
+    if((n1 > n3) && (n1> n2)) {
+        printf("O maior numero e: %0.2f", n1);
+    }
+    if((n2 > n3) && (n2 > n1)) {
+        printf("O maior numero e: %0.2f", n2);
+    }
+    if((n3 > n1) && (n3 > n2)) {
+        printf("O maior numero e: %0.2f", n3);
+    }
+    if((n1 == n2) || (n2 == n3) || (n1 == n3)) {
+        printf("Os numeros devem ser diferentes!");
+        return 1;
+    }
+
+    return 0;
+}
