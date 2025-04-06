fb621c16a15ce0e8d984430b90e1db01ac0ba2e9
moraissdev <renanbusinessdev@gmail.com>
2025-04-06 19:01:17 -0300

    commit

---
+++ sistemaInternoEmpresa.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int codigoDeAcesso;
+    printf("Digite seu codigo de acesso (numero inteiro): ");
+    scanf("%d", &codigoDeAcesso);
+
+    if(codigoDeAcesso == 1) {
+        printf("Acesso permitido (Administrador)");
+    }
+    if(codigoDeAcesso == 2) {
+        printf("Acesso permitido (Cracha Ativo)");
+    }
+    if((codigoDeAcesso != 1) && (codigoDeAcesso != 2)) {
+        printf("Acesso negado");
+    }
+
+    return 0;
+}
