e2073bbd5d2a5fd816fb9fed7009d8976ccd547a
moraissdev <renanbusinessdev@gmail.com>
2025-04-06 18:10:00 -0300

    commit

---
+++ numeroParOuImpar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n1;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &n1);
+
+    if(n1 % 2 == 0) { //SE O RESTO DA DIVISÃO POR 2 POR 0, ELE EXECUTA O QUE ESTÁ DENTRO
+        printf("%d e par", n1);
+    }
+    else {
+        printf("%d e impar", n1);
+    }
+}
