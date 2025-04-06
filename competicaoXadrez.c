87fcdd82d73e8509213db9bb839390455190606c
moraissdev <renanbusinessdev@gmail.com>
2025-04-06 18:21:45 -0300

    commit

---
+++ competicaoXadrez.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int nascimento, res;
+    printf("Digite seu ano de nascimento: ");
+    scanf("%d", &nascimento);
+
+    res = 2025 - nascimento;
+    printf("Idade = %d\n", res);
+    if ((res >= 12) && (res <= 15)) {
+        printf("Apto a participar do campeonato");
+    }
+    else {
+        printf("Nao pode participar do campeonato");
+    }
+    return 0;
+}
