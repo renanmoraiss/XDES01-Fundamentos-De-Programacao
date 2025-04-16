4e88acf9aeca4e6dfd25a8c1ec54a60079b1eb85
moraissdev <renanbusinessdev@gmail.com>
2025-04-15 23:08:04 -0300

    commit

--- mediaAritmetica.c
+++ mediaAritmetica.c
@@ -2,7 +2,7 @@
 
 int main() {
     int n1, n2, n3;
-    float res;
+    double res;
     printf("Digite o primeiro valor: ");
     scanf("%d", &n1);
     printf("Digite o segundo valor: ");
@@ -10,6 +10,6 @@ int main() {
     printf("Digite o terceiro valor: ");
     scanf("%d", &n3);
     res = (n1 + n2 + n3) / 3;
-    printf("A media e: %0.2f", res);
+    printf("A media e: %0.2lf", res);
     return 0;
 }
