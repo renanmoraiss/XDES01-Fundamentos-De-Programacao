d368d853cfe378e1a0611bf23bf6f62225318ea0
moraissdev <renanbusinessdev@gmail.com>
2025-04-20 19:32:17 -0300

    commit

--- imprimirTimeDeFutsalDinamico.c
+++ imprimirTimeDeFutsalDinamico.c
@@ -18,6 +18,8 @@ int main() {
     int idadeInferiorA18 = 0;
     char continuarOuParar;
     int limite;
+    float maiorAltura = 0.0;
+    float menorAltura = 0.0;
 
     printf("-----------------------------\n");
     printf("ALGORITMO PARA CALCULAR ESTATISTICAS\n");
@@ -36,6 +38,11 @@ int main() {
     printf("ALTURA: ");
     scanf("%f", &altura);
     printf("PESO: ");
+    if (altura > maiorAltura) {
+        maiorAltura = altura;
+    } else if (altura < menorAltura) {
+        menorAltura = altura;
+    }
     scanf("%f", &peso);
     if (peso > 80) {
         pesoAcimaDe80kg += 1;
@@ -55,12 +62,14 @@ int main() {
     } else {
         printf("NENHUM JOGADOR COM IDADE INFERIOR A 18 ANOS\n");
     }
+    printf("MAIOR ALTURA: %0.2f METRO\n", maiorAltura);
+    printf("MENOR ALTURA: %0.2f METRO\n", menorAltura);
     mediaDasIdades = (float) somaDasIdades / limite;
-    printf("MEDIA DAS IDADES: %0.1f ANOS\n", mediaDasIdades);
+    printf("MEDIA DAS IDADES: %0.2f ANOS\n", mediaDasIdades);
     mediaDasAlturas = somaDasAlturas / limite;
-    printf("MEDIA DAS ALTURAS: %0.1f METROS\n", mediaDasAlturas);
+    printf("MEDIA DAS ALTURAS: %0.2f METROS\n", mediaDasAlturas);
     mediaDosPesos = somaDosPesos / limite;
-    printf("MEDIA DOS PESOS: %0.1f KG\n", mediaDosPesos);
+    printf("MEDIA DOS PESOS: %0.2f KG\n", mediaDosPesos);
     porcentagemJogadoresPesoAcima80kg = ((float)pesoAcimaDe80kg / limite) * 100;
     if (pesoAcimaDe80kg == 1) {
     printf("%d JOGADOR ACIMA DE 80 KG\n", pesoAcimaDe80kg);
