1d2077111e8b035fcb42f9a4e760070f7bed04ab
moraissdev <renanbusinessdev@gmail.com>
2025-05-10 15:48:23 -0300

    commit

---
+++ lista6questao1MOODLE.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void soma(int *n1, int *n2);
+
+int main() {
+    int nmr1, nmr2;
+    //
+    scanf("%d", &nmr1);
+    //
+    scanf("%d", &nmr2);
+    //
+    printf("Antes da função\n");
+    printf("A: %d B: %d\n", nmr1, nmr2);
+    //
+    soma(&nmr1, &nmr2);
+    //
+    printf("Depois da função\n");
+    printf("A: %d B: %d\n", nmr1, nmr2);
+    //
+    return 0;
+}
+
+void soma(int *n1, int *n2) {
+    *n1 = *n1 + *n2;
+}
---
+++ lista6questao6MOODLE.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    //Declaração padrão de inteiros
+    int a = 4, b = 7, c = 2;
+    //
+    //Declaração de ponteiros inteiros (ponteiro armazena um endereço de memória)
+    int *p = &a; //ponteiro *p aponta para o endereço de memória de a, que é 4.
+    int *q = &b; //ponteiro *q aponta para o endereço de memória de b, que é 7.
+    int *r = &c; //ponteiro *r aponta para o endereço de memória de c, que é 2.
+    //
+    *p = *q - *r; // => a = b - c => a = 7 - 2 = 5. (a = 5)
+    r = p; // r = &a (r aponta para o endereço de a agora). => r e p apontam para o mesmo endereço.
+    *q = *r + *q; // => b = a + b => b = 5 + 7 = 12. (b = 12)
+    //c = 2 (não foi alterado em nenhuma operação).
+    //
+    printf("%d %d %d\n", a, b, c);
+    //
+    return 0;
+}
