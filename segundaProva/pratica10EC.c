820c053dfe744f6d3a2cd308d0f99fe37fd6b6cb
moraissdev <renanbusinessdev@gmail.com>
2025-05-24 16:08:15 -0300

    commit

---
+++ segundaProva/pratica10EC.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void preencheValor(float *valor1, float *valor2);
+float retornaMenor(float *valor1, float *valor2);
+
+int main() {
+    float n1, n2;
+    float menor;
+    //
+    preencheValor(&n1, &n2);
+    //
+    menor = retornaMenor(&n1, &n2);
+    printf("Menor: %0.2f\n", menor);
+    //
+    return 0;
+}
+
+void preencheValor(float *valor1, float *valor2) {
+    printf("Digite o primeiro valor: ");
+    scanf("%f", valor1);
+    //
+    printf("Digite o segundo valor: ");
+    scanf("%f", valor2);
+}
+
+float retornaMenor(float *valor1, float *valor2) {
+    if (*valor1 < *valor2) {
+        return *valor1;
+    } else {
+        return *valor2;
+    }
+}
--- segundaProva/pratica9EC.c
+++ segundaProva/pratica9EC.c
@@ -1,2 +1,51 @@
 #include <stdio.h>
 
+void preencheDados(float *a, float *p);
+void verificaDados(float *a, float *p);
+
+int main() {
+    float altura, peso;
+    //
+    preencheDados(&altura, &peso);
+    //
+    verificaDados(&altura, &peso);
+    //
+    return 0;
+}
+
+void preencheDados(float *a, float *p) {
+    printf("Digite seu peso: ");
+    scanf("%f", p);
+    //
+    printf("Digite sua altura: ");
+    scanf("%f", a);
+    //
+}
+
+void verificaDados(float *a, float *p) {
+    if (*p < 60) {
+        if (*a < 1.20) {
+            printf("A\n");
+        } else if ((*a >= 1.20) && (*a <= 1.70)) {
+            printf("B\n");
+        } else {
+            printf("C\n");
+        }
+    } else if ((*p >= 60) && (*p <= 90)) {
+        if (*a < 1.20) {
+            printf("D\n");
+        } else if ((*a >= 1.20) && (*a <= 1.70)) {
+            printf("E\n");
+        } else {
+            printf("F\n");
+        }
+    } else {
+        if (*a < 1.20) {
+            printf("G\n");
+        } else if ((*a >= 1.20) && (*a <= 1.70)) {
+            printf("H\n");
+        } else {
+            printf("I\n");
+        }
+    }
+}
