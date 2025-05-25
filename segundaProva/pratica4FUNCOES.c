86041c1f26c2c3446427526d75a428cfc8e99ac3
moraissdev <renanbusinessdev@gmail.com>
2025-05-25 19:35:58 -0300

    commit

---
+++ segundaProva/pratica4FUNCOES.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+float calculoMedia(float n1, float n2, float n3, char escolha); //passagem de parametro por valor (copia)
+
+int main() {
+    float nota1, nota2, nota3;
+    char tipoDeMedia;
+    float media;
+    //
+    printf("Digite a nota 1: ");
+    scanf("%f", &nota1);
+    //
+    printf("Digite a nota 2: ");
+    scanf("%f", &nota2);
+    //
+    printf("Digite a nota 3: ");
+    scanf("%f", &nota3);
+    //
+    scanf(" %c", &tipoDeMedia);
+    //
+    media = calculoMedia(nota1, nota2, nota3, tipoDeMedia);
+    printf("%0.2f\n", media);
+    //
+    return 0;
+}
+
+float calculoMedia(float n1, float n2, float n3, char escolha) {
+    float mediaA, mediaP;
+    if (escolha == 'A') {
+        mediaA = (n1 + n2 + n3) / 3;
+        return mediaA;
+    } else if (escolha == 'P') {
+        mediaP = ((n1 * 5) + (n2 * 3) + (n3 * 2)) / 10;
+        return mediaP;
+    } else if (escolha != 'A' && escolha != 'P') {
+        return -1;
+    }
+}
---
+++ segundaProva/pratica5FUNCOES.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+float calculoExponencial(float base, int expoente); //passagem de parametro por valor (copia)
+
+int main() {
+    float x, res;
+    int y;
+    //
+    printf("Base: ");
+    scanf("%f", &x);
+    //
+    printf("Expoente: ");
+    scanf("%d", &y);
+    //
+    res = calculoExponencial(x, y);
+    printf("%0.2f\n", res);
+    //
+    return 0;
+}
+
+float calculoExponencial(float base, int expoente) {
+    float calc = 1.0;
+    //
+    for (int i = 1; i <= expoente; i++) {
+        calc *= base;
+    } return calc;
+}
