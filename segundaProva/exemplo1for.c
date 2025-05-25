50ddcdd9b4699feab38531ad1d366a451b657410
moraissdev <renanbusinessdev@gmail.com>
2025-05-25 12:06:00 -0300

    commit

---
+++ segundaProva/exemplo1for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+//int i = 1 -> declara e inicializa a variável de controle
+//i <= 10 -> testa a condição para permanecer no loop
+//i++ -> incrementa a variável de controle
---
+++ segundaProva/exemplo1while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int contador;
+    int n;
+    //
+    printf("Digite um valor inteiro: ");
+    scanf("%d", &n);
+    //
+    contador = 1;
+    //
+    while (contador <= n) {
+        printf("%d\n", contador);
+        contador++;
+    }
+    //
+    return 0;
+}
