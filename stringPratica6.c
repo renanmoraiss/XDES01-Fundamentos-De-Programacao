126b54ad9d4e56d0d2e04f87ccc0664722e282cf
moraissdev <renanbusinessdev@gmail.com>
2025-06-07 22:31:15 -0300

    praticando String para a prova

---
+++ stringPratica6.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+void mensagem();
+void leMensagem(char frase[]);
+void calcularTamanho(char frase[], int *tamanho);
+void inverterMensagem(char frase[], int *tamanho);
+
+int main() {
+    char string[30];
+    int tam;
+    //
+    mensagem();
+    //
+    leMensagem(string);
+    //
+    calcularTamanho(string, &tam);
+    //
+    inverterMensagem(string, &tam);
+    printf("%s\n", string);
+    //
+    return 0;
+}
+
+void mensagem() {
+    printf("Digite uma mensagem\n");
+}
+
+void leMensagem(char frase[]) {
+    scanf("%[^\n]", frase);
+}
+
+void calcularTamanho(char frase[], int *tamanho) {
+    *tamanho = 0;
+    int i = 0;
+    //
+    while (frase[i] != '\0') {
+        (*tamanho)++;
+        i++;
+    }
+}
+
+void inverterMensagem(char frase[], int *tamanho) {
+    char caracter;
+    for (int i = *tamanho - 1, j = 0; j < i; i--, j++) {
+        caracter = frase[j]; 
+        frase[j] = frase[i]; 
+        frase[i] = caracter;
+    }
+}
---
+++ stringPratica7.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <string.h>
+
+void mensagem();
+void leMensagem(char frase[]);
+void escolheCaracter(char *caracterEscolhido);
+void tamanhoDaMensagem(char frase[], int *tam);
+void contarVogaisEsubstituir(char frase[], char *caracterEscolhido, int *tam, int *totalVogais);
+int main() {
+    char string[30];
+    int tamanho;
+    int totalVogais;
+    char caracter;
+    //
+    mensagem();
+    //
+    leMensagem(string);
+    //
+    escolheCaracter(&caracter);
+    //
+    tamanhoDaMensagem(string, &tamanho);
+    //
+    contarVogaisEsubstituir(string, &caracter, &tamanho, &totalVogais);
+    //
+    return 0;
+}
+
+void mensagem() {
+    printf("Digite uma mensagem\n");
+}
+
+void leMensagem(char frase[]) {
+    scanf("%[^\n]", frase);
+}
+
+void escolheCaracter(char *caracter) {
+    printf("Digite um caracter\n");
+    scanf(" %c", caracter);
+}
+
+void tamanhoDaMensagem(char frase[], int *tam) {
+    *tam = 0;
+    int i = 0;
+    while (frase[i] != '\0') {
+        (*tam)++;
+        i++;
+    }
+}
+
+void contarVogaisEsubstituir(char frase[], char *caracterEscolhido, int *tam, int *totalVogais) {
+    *totalVogais = 0;
+    for (int i = 0; i < *tam; i++) {
+        if (frase[i] == 'a') {
+            frase[i] = *caracterEscolhido;
+            (*totalVogais)++;
+        } else if (frase[i] == 'e') {
+            frase[i] = *caracterEscolhido;
+            (*totalVogais)++;
+        } else if (frase[i] == 'i') {
+            frase[i] = *caracterEscolhido;
+            (*totalVogais)++;
+        } else if (frase[i] == 'o') {
+            frase[i] = *caracterEscolhido;
+            (*totalVogais)++;
+        } else if (frase[i] == 'u') {
+            frase[i] = *caracterEscolhido;
+            (*totalVogais)++;
+        }
+    }
+    printf("%s\n", frase);
+    printf("Total de vogais: %d\n", *totalVogais);
+}
