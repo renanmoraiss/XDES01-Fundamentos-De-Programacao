489e4dadbfba4e353f920a390d85d4a00d3ead51
moraissdev <renanbusinessdev@gmail.com>
2025-06-09 07:20:53 -0300

    commit

--- livroFundDeProg3ªedicao/capitulo6-VETOR/exercicio1.c
+++ livroFundDeProg3ªedicao/capitulo6-VETOR/exercicio1.c
@@ -4,15 +4,23 @@ primos e suas respectivas posições.*/
 #include <stdio.h>
 
 void preencheVetor(int vet[], int tamanho);
+void calcularPrimos(int vet[], int tamanho);
 
 int main() {
     int vetor[8];
     //
     preencheVetor(vetor, 8);
     //
+    calcularPrimos(vetor, 8);
+    //
     return 0;
 }
 
 void preencheVetor(int vet[], int tamanho) {
-    for (int i = 0;)
+    for (int i = 0; i <= tamanho; i++) {
+        scanf("%d", &vet[i]);
+    }
+}
+
+void calcularPrimos(int vet[], int tamanho) {
 }
