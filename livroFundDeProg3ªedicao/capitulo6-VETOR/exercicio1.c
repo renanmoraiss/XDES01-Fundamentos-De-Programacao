7f040279e6f96cb71ea5425b740bf21f9fe6c336
moraissdev <renanbusinessdev@gmail.com>
2025-06-09 06:32:14 -0300

    commit

---
+++ livroFundDeProg3ªedicao/capitulo6-VETOR/exercicio1.c
@@ -0,0 +1,18 @@
+/*Faça um programa que preencha um vetor com nove números inteiros, calcule e mostre os números
+primos e suas respectivas posições.*/
+
+#include <stdio.h>
+
+void preencheVetor(int vet[], int tamanho);
+
+int main() {
+    int vetor[8];
+    //
+    preencheVetor(vetor, 8);
+    //
+    return 0;
+}
+
+void preencheVetor(int vet[], int tamanho) {
+    for (int i = 0;)
+}
