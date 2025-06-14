8da3fd6ac701adec5b417dd8ff063cabf23237f2
moraissdev <renanbusinessdev@gmail.com>
2025-06-14 20:30:05 -0300

    commit

--- moodleStruct.c
+++ moodleStruct.c
@@ -13,12 +13,12 @@ typedef struct cadastro Aluno;
 int cadastrarAlunos(Aluno alunos[]); //função 1
 float calcularMedia(Aluno aluno); //função 2
 void encontrarMelhorAluno(Aluno alunos[], int n); //função 3
+void carregaMatriz(Aluno alunos[], int totalAlunos, float mat[][6]); //função 4
 
 int main() {
     Aluno alunos[100];
     int totalAlunos;
-    Aluno alunoMedia;
-    int alunoEscolhidoParaCalcularMedia;
+    float dadosAlunos[100][6];
     //
     totalAlunos = cadastrarAlunos(alunos);
     //
@@ -28,6 +28,8 @@ int main() {
     }
     //
     encontrarMelhorAluno(alunos, totalAlunos);
+    //
+    carregaMatriz(alunos, totalAlunos, dadosAlunos);
     //
     return 0;
 }
@@ -67,6 +69,7 @@ float calcularMedia(Aluno aluno) {
 void encontrarMelhorAluno(Aluno alunos[], int n) {
     float melhorMedia = alunos[0].media;
     char nome[50];
+    strcpy(nome, alunos[0].nome);
     //
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < 4; j++) {
@@ -77,4 +80,22 @@ void encontrarMelhorAluno(Aluno alunos[], int n) {
         }
     }
     printf("Nome: %s -- Media: %0.2f\n", nome, melhorMedia);
+}
+
+void carregaMatriz(Aluno alunos[], int totalAlunos, float mat[][6]) {
+    //
+    int linhas;
+    linhas = totalAlunos;
+    //
+    //PREENCHER O ID DO ALUNO
+    for (int i = 0; i < linhas; i++) {
+        //
+        for (int j = 0; j < 6; j++) {
+            mat[i][0] = i; //PREENCHE ID's
+            //
+            mat[i][j+1] = alunos[i].notas[j]; //PREENCHE NOTAS
+            //
+            mat[i][5] = alunos[i].media; //PREENCHE MÉDIAS
+        }
+    }
 }
