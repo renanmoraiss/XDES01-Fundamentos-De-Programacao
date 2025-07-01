a4deb9a8cfcc7397b35bf7794f5e971d60346a97
moraissdev <renanbusinessdev@gmail.com>
2025-07-01 00:23:38 -0300

    manipulação de arquivos

--- exemplo4arquivo.c
+++ exemplo4arquivo.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+
+int main() {
+    char nome[30];
+    int tel;
+    //
+    FILE *arq;
+    //
+    arq = fopen("cadastro.txt", "w");
+    if (arq == NULL) {
+        return 1;
+    }
+    printf("Nome: ");
+    scanf("%s", nome);
+    //
+    printf("Telefone: ");
+    scanf("%d", &tel);
+    //
+    while (tel != 0) {
+        fprintf(arq, "%s\t%d\n", nome, tel);
+        //
+        printf("Nome: ");
+        scanf("%s", nome);
+        //
+        printf("Telefone: ");
+        scanf("%d", &tel);
+    }
+    fclose(arq);
+    //
+    return 0;
+}
